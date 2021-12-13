3c9327b2454bc6d4cc4abe818ba46a05d5246738
EdenGenetTasama <edentasama19@gmail.com>
2021-12-13 10:58:26 +0200

    add StudentsView

---
+++ SchoolApp/Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+﻿using SchoolApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolApp.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult Students()
+        {
+            Student studentOne = new Student() ;
+            studentOne.FirstName = "Eden";
+            studentOne.LastName = "Tasama";
+            studentOne.YearOfBirth = 1996;
+            ViewBag.Student = studentOne ;
+            return View();
+        }
+
+       
+    }
+}
---
+++ SchoolApp/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolApp.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int YearOfBirth { get; set; }
+        public int Class { get; set; }
+
+    }
+}
